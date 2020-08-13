730566231920d7cdc9584e33de9381b70e66c7b5
Alan <hu.ala@northeastern.edu>
2020-08-13 19:17:12 +0000

    Scrap idea of tracking scopes in types

--- LazySlice/Normalize.hs
+++ LazySlice/Normalize.hs
@@ -1,28 +1,21 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 module LazySlice.Normalize where
 
 import LazySlice.Syntax
-
-shift :: Val v -> Val (Maybe v)
-shift (Free v) = Free (Just v)
-shift (Clos rho t) = Clos (Binding rho (Free Nothing)) (fmap Just t)
-
-getVar :: Env v -> v -> Val v
-getVar (Binding _ val) Nothing = val
-getVar (Binding rho _) (Just v) = shift $ getVar rho v
+import Control.Monad.Except (MonadError, throwError)
 
 -- | Evaluate to WHNF.
-whnf :: Env v -> Term v -> Whnf v
-whnf rho (App t u) = app (whnf rho t) (Clos rho u)
-whnf rho (Lam a t) = WLam rho (Clos rho a) t
-whnf rho (Pi a b) = WPi (Clos rho a) (Clos (Binding rho (Free Nothing)) b)
-whnf rho (Sigma a b) = WSigma (Clos rho a) (Clos (Binding rho (Free Nothing)) b)
-whnf _ Universe = WUniverse
-whnf rho (Var v) =
-    case getVar rho v of
-        Clos rho' t -> whnf rho' t
-        Free v -> WNeu (NVar v)
-
-app :: Whnf v -> Val v -> Whnf v
-app (WLam rho a t) v = WLet v (whnf (Binding rho (shift v)) t)
-app (WNeu neu) v = WNeu (NApp neu v)
+whnf :: MonadError String m => Env -> Term -> m Whnf
+whnf rho (App t u) = do
+    t <- whnf rho t
+    case t of
+        WLam a (Clos rho t) -> whnf (v : rho) t
+        WNeu neu -> return $ WNeu (NApp neu v)
+        _ -> throwError "Not a function"
+    where v = Clos rho u
+whnf rho (Lam a t) = return $ WLam (Clos rho a) (Clos rho t)
+whnf rho (Pi a b) = return $ WPi (Clos rho a) (Clos rho b)
+whnf rho (Sigma a b) = return $ WSigma (Clos rho a) (Clos rho b)
+whnf _ Universe = return WUniverse
+whnf rho (Var v) = whnf rho' t
+    where Clos rho' t = rho !! v
--- LazySlice/Syntax.hs
+++ LazySlice/Syntax.hs
@@ -1,43 +1,29 @@
-{-# LANGUAGE DeriveFunctor, GADTs, KindSignatures #-}
 module LazySlice.Syntax where
 
 -- | http://www.cse.chalmers.se/~abela/msfp08.pdf is a good guide.
+--  http://www.davidchristiansen.dk/tutorials/nbe/ presents similar code in Racket.
 
--- | The uninhabited type.
-data Void :: * where
-
--- | Terms are parameterized by v, the type of their variables.
---   Whenever a new scope is entered, v is increased by one
---   inhabitant.
-data Term v
-    = App (Term v) (Term v)
-    | Lam (Term v) (Term (Maybe v))
-    | Pi (Term v) (Term (Maybe v))
-    | Sigma (Term v) (Term (Maybe v))
+data Term
+    = App Term Term
+    | Lam Term Term
+    | Pi Term Term
+    | Sigma Term Term
     | Universe
-    | Var v
-    deriving Functor
+    | Var Int
 
 -- | A spine of function applications.
-data Neutral v
-    = NApp (Neutral v) (Val v)
-    | NVar v
+data Neutral
+    = NApp Neutral Val
+    | NVar Int
 
 -- | Weak head normal forms.
-data Whnf v
-    = WNeu (Neutral v)
-    | WLam (Env v) (Val v) (Term (Maybe v))
-    | WLet (Val v) (Whnf (Maybe v))
-    | WPi (Val v) (Val (Maybe v))
-    | WSigma (Val v) (Val (Maybe v))
+data Whnf
+    = WNeu Neutral
+    | WLam Val Val
+    | WPi Val Val
+    | WSigma Val Val
     | WUniverse
 
--- | An environment is a map from variables to their values. It is not to be confused
---   with a typing context, which is a map from variables to their types.
-data Env :: * -> * where
-    Empty :: Env Void
-    Binding :: Env v -> Val (Maybe v) -> Env (Maybe v)
+type Env = [Val]
 
-data Val v
-    = Clos (Env v) (Term v) -- ^ A closure is a term in an environment.
-    | Free v
+data Val = Clos Env Term
