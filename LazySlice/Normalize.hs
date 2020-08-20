6e108af6592708922261820708130ba4a0201f2b
Alan <hu.ala@northeastern.edu>
2020-08-20 05:15:24 +0000

    Use concrete monad

--- LazySlice/Normalize.hs
+++ LazySlice/Normalize.hs
@@ -1,57 +1,64 @@
-{-# LANGUAGE FlexibleContexts, FunctionalDependencies #-}
+{-# LANGUAGE FlexibleContexts, FunctionalDependencies, GeneralizedNewtypeDeriving #-}
 module LazySlice.Normalize where
 
 import LazySlice.Syntax
-import Control.Monad.Cont (MonadCont, callCC)
 import Control.Monad.Except (MonadError, throwError)
-import Control.Monad.Trans.Cont (Cont)
+import Control.Monad.Reader (MonadReader, ask, local)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Cont (ContT, resetT, shiftT)
+import Control.Monad.Trans.Except (ExceptT(..))
+import Control.Monad.Trans.Reader (ReaderT)
 
-class Monad m => MonadEval m where
-    inScope :: (Int -> m a) -> m a
-    reset :: m (Whnf m) -> m (Whnf m)
-    shift :: ((Whnf m -> m (Whnf m)) -> m (Whnf m)) -> m (Whnf m)
+inScope :: (Int -> Eval a) -> Eval a
+inScope f = do
+    idx <- ask
+    local (+1) (f $ idx + 1)
+
+prompt :: Eval Whnf -> Eval Whnf
+prompt (Eval m) = Eval $ resetT m
+
+control :: ((Either String Whnf -> ContTy Whnf) -> Eval Whnf) -> Eval Whnf
+control f = Eval $ shiftT (eval . f)
+
+typeError :: String -> Eval a
+typeError = Eval . pure . Left
 
 handler _ = Nothing
 
 -- | Evaluate to WHNF.
-whnf
-    :: (MonadEval m, MonadError String m)
-    => Env m -> Conts m -> Term -> Handler -> m (Whnf m)
+whnf :: Env -> Conts -> Term -> Handler -> Eval Whnf
 whnf rho kappa (App t u) h = do
     t <- whnf rho kappa t h
     case t of
         WLam a (Abs rho t) -> whnf (Val v : rho) kappa t h
         WNeu neu -> return $ WNeu (NApp neu v)
-        _ -> throwError "Not a function"
+        _ -> typeError "Not a function"
     where v = Clos rho kappa h u
 whnf rho kappa (Break t u) h = do
     t <- whnf rho kappa t h
     case t of
         WCont k -> do
             u <- whnf rho kappa u h
-            k u
-        _ -> throwError "Not a continuation"
+            Eval $ lift $ (k (Right u))
+        _ -> typeError "Not a continuation"
 whnf _ kappa (Cont k) _ = return $ WCont (kappa !! k)
 whnf rho kappa (Lam a t) h =
     return $ WLam (Clos rho kappa h a) (Abs rho t)
 whnf rho kappa (Raise eff) h =
-    shift $ \k ->
+    control $ \k ->
         case h eff of
-            Nothing -> throwError $ "Uncaught exception: " ++ eff
+            Nothing -> typeError $ "Uncaught exception: " ++ eff
             Just t -> whnf rho (k : kappa) t handler
 whnf rho kappa (Pi a b) h = return $ WPi (Clos rho kappa h a) (Abs rho b)
 whnf rho kappa (Sigma a b) h = return $ WSigma (Clos rho kappa h a) (Abs rho b)
-whnf rho kappa (Try t) h =
-    reset (whnf rho kappa t h)
+whnf rho kappa (Try t) h = prompt (whnf rho kappa t h)
 whnf _ _ Universe _ = return WUniverse
 whnf rho _ (Var v) _ =
     case rho !! v of
         Val (Clos rho' kappa' h t) -> whnf rho' kappa' t h
         Free v -> return $ WNeu (NVar v)
 
-unifyWhnf
-    :: (MonadEval m, MonadError String m)
-    => Whnf m -> Whnf m -> m ()
+unifyWhnf :: Whnf -> Whnf -> Eval ()
 unifyWhnf (WNeu neul) (WNeu neur) = unifyNeu neul neur
 unifyWhnf (WLam a t) (WLam b u) = unifyAbs t u
 unifyWhnf (WPi a b) (WPi c d) = do
@@ -61,29 +68,24 @@ unifyWhnf (WSigma a b) (WSigma c d) = do
     unifyVal a c
     unifyAbs b d
 unifyWhnf WUniverse WUniverse = return ()
-unifyWhnf _ _ = throwError "Unify fail"
+unifyWhnf _ _ = typeError "Unify fail"
 
-unifyNeu :: (MonadEval m, MonadError String m)
-    => Neutral m -> Neutral m -> m ()
+unifyNeu :: Neutral -> Neutral -> Eval ()
 unifyNeu (NVar i) (NVar j)
     | i == j = return ()
-    | otherwise = throwError "Unify fail"
+    | otherwise = typeError "Unify fail"
 unifyNeu (NApp nl vl) (NApp nr vr) = do
     unifyNeu nl nr
     unifyVal vl vr
-unifyNeu _ _ = throwError "Unify fail"
+unifyNeu _ _ = typeError "Unify fail"
 
-unifyVal
-    :: (MonadEval m, MonadError String m)
-    => Val m -> Val m -> m ()
+unifyVal :: Val -> Val -> Eval ()
 unifyVal (Clos rho kappa h t) (Clos rho' kappa' h' u) = do
     t <- whnf rho kappa t h
     u <- whnf rho' kappa' u h'
     unifyWhnf t u
 
-unifyAbs
-    :: (MonadEval m, MonadError String m)
-    => Abs m -> Abs m -> m ()
+unifyAbs :: Abs -> Abs -> Eval ()
 unifyAbs (Abs rho t) (Abs rho' u) =
     inScope $ \i -> do
         t <- whnf (Free i : rho) [] t handler
--- LazySlice/Syntax.hs
+++ LazySlice/Syntax.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE FlexibleContexts, FunctionalDependencies, GeneralizedNewtypeDeriving #-}
 module LazySlice.Syntax where
+
+import Control.Monad.Except (MonadError, throwError)
+import Control.Monad.Reader (MonadReader, ask, local)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Cont (ContT, resetT)
+import Control.Monad.Trans.Except (ExceptT(..))
+import Control.Monad.Trans.Reader (Reader)
 
 -- | http://www.cse.chalmers.se/~abela/msfp08.pdf is a good guide.
 --  http://www.davidchristiansen.dk/tutorials/nbe/ presents similar code in Racket.
@@ -15,33 +23,55 @@ data Term
     | Universe
     | Var Int -- ^ A variable is a De Bruijn index (which counts from the inside-out).
 
+type ContTy a = (Reader Int) (Either String a)
+
+newtype Eval a = Eval
+    { eval :: ContT (Either String Whnf) (Reader Int) (Either String a) }
+
+instance Functor Eval where
+    fmap f (Eval e) = Eval $ fmap (fmap f) e
+
+instance Applicative Eval where
+    pure a = Eval (pure $ pure a)
+    (Eval f) <*> (Eval a) = Eval $ (<*>) <$> f <*> a
+
+instance Monad Eval where
+    (Eval a) >>= f = Eval $ a >>= go
+        where
+            go (Left e) = pure (Left e)
+            go (Right a) = eval $ f a
+
+instance MonadReader Int Eval where
+    ask = Eval $ fmap Right ask
+    local f (Eval e) = Eval (local f e)
+
 -- | A spine of function applications.
-data Neutral m
-    = NApp (Neutral m) (Val m)
+data Neutral
+    = NApp Neutral Val
     | NVar Int
 
 -- | Weak head normal forms.
-data Whnf m
-    = WCont (Whnf m -> m (Whnf m))
-    | WNeu (Neutral m)
-    | WLam (Val m) (Abs m)
-    | WPi (Val m) (Abs m)
-    | WSigma (Val m) (Abs m)
+data Whnf
+    = WCont (Either String Whnf -> ContTy Whnf)
+    | WNeu Neutral
+    | WLam Val Abs
+    | WPi Val Abs
+    | WSigma Val Abs
     | WUniverse
 
 -- | The environment of values.
-type Env m = [Binding m]
+type Env = [Binding]
 
 -- | The environment of continuations.
-type Conts m = [Whnf m -> m (Whnf m)]
+type Conts = [Either String Whnf -> ContTy Whnf]
 
-data Binding m
-    = Val (Val m)
+data Binding
+    = Val Val
     | Free Int -- ^ A free variable is not a De Bruijn index, and it counts from the outside in.
 
 -- | A handler catches an effect.
 type Handler = String -> Maybe Term
 
-data Val m = Clos (Env m) (Conts m) Handler Term
+data Val = Clos Env Conts Handler Term
 
-data Abs m = Abs (Env m) Term
+data Abs = Abs Env Term
